688f11d6619f9e40b7610a179f0732aafc8d6c3a
FabricioSMarin <fabricio.s.marin@gmail.com>
2025-03-02 13:15:05 -0600

    added line-edited functions
    Do x when y_isclicked

--- Mothy.py
+++ Mothy.py
@@ -3,11 +3,11 @@ from PyQt5.QtCore import Qt, QSettings
 import sys
 import serial
 import time
+import numpy as np
 from motor import MotorSettings
 from cam import Controls
 from dpad import DPad
 from visuals import ImagePlotWidget
-import numpy as np
 
 
 class MainWindow(QMainWindow):
@@ -19,6 +19,7 @@ class MainWindow(QMainWindow):
         self.tracked_points = 0
         self.initUI()
         self.loadSettings()
+        self.update_settings()
 
     def initUI(self):
         central_widget = QWidget()
@@ -58,6 +59,8 @@ class MainWindow(QMainWindow):
         central_widget.setLayout(self.main_layout)
         self.setCentralWidget(central_widget)
         self.setWindowTitle("PyQt5 Main Window with SimpleWidgets")
+        
+
 
     def closeEvent(self, event):
         """ Triggered when the window is closed, used to save settings. """
@@ -93,6 +96,7 @@ class MainWindow(QMainWindow):
         self.settings.setValue("num_images", self.camera_controls.num_images_edit.text())
         self.settings.setValue("mode", self.camera_controls.mode_combobox.currentIndex())
 
+
     def loadSettings(self):
         """ Load the QLineEdit contents from QSettings """
         self.dpad.ud_lineedit.setText(self.settings.value("ud_text", ""))
@@ -122,6 +126,42 @@ class MainWindow(QMainWindow):
         self.camera_controls.num_images_edit.setText(self.settings.value("num_images", ""))
         self.camera_controls.mode_combobox.setCurrentIndex(self.settings.value("mode", 0))
 
+        
+    def update_settings(self): 
+        attrs = {"res":"Resolution","velo":"Velocity","acc":"Acceleration","bac":"Backlash"}
+        for motor in [self.motor1, self.motor2, self.motor3]
+            for attr in attrs.keys():
+                try:
+                    setattr(motor, attr, eval(fields[attrs[attr]].text())
+                except:
+                    setattr(motor, attr, None)
+
+    def update_resolution(self, motor):
+        try:
+            setattr(motor, "res", eval(fields["Resolution"].text())
+        except:
+            setattr(motor, "res", None)
+        
+    def update_velocity(self, motor):
+        try:
+            setattr(motor, "velo", eval(fields["Velocity"].text())
+        except:
+            setattr(motor, "velo", None)
+        
+        
+    def update_acceleration(self, motor):
+        try:
+            setattr(motor, "acc", eval(fields["Acceleration"].text())
+        except:
+            setattr(motor, "acc", None)
+        
+    def update_backlash(self, motor):
+        try:
+            setattr(motor, "bac", eval(fields["Backlash"].text())
+        except:
+            setattr(motor, "bac", None)
+      
+         
     def adjust_size(self):
         """ Dynamically resize main window based on visible widgets. """
         total_height = 0
@@ -155,60 +195,34 @@ class MainWindow(QMainWindow):
         else:
             self.esp32_connected=False
 
-    def connect_camera(self):
-        pass
-
-    def up_clickecd(self):
-        resolition,velocity,acceltime,backlash = self.get_settings(self.motor1)
-        steps = self.dpad.ud_lineedit.text()
-        motor="1"
-        direction= "1"
-        command ="{motor},{resolution},{direction},{steps},{velocity},{acceltime},{backlash}\n"
+    def up_clicked(self):
+        #"{motor_num},{resolution},{direction},{steps},{velocity},{acceltime},{backlash}\n"
+        command ="{1},{self.motor1.res},{1},{self.dpad.ud_lineedit.text()},{self.motor1.velo},{self.motor1.acc},{self.motor1.bac}\n" 
         self.esp32.write(command.encode())
-        
 
     def down_clicked(self):
-        resolition,velocity,acceltime,backlash = self.get_settings(self.motor1)
-        steps = self.dpad.ud_lineedit.text()
-        motor="1"
-        direction="0"
-        command ="{motor},{resolution},{direction},{steps},{velocity},{acceltime},{backlash}\n"
+        command ="{1},{self.motor1.res},{0},{self.dpad.ud_lineedit.text()},{self.motor1.velo},{self.motor1.acc},{self.motor1.bac}\n" 
         self.esp32.write(command.encode())
 
     def left_clicked(self):
-        resolition,velocity,acceltime,backlash = self.get_settings(self.motor2)
-        steps = self.dpad.lr_lineedit.text()
-        motor="2"
-        direction="0"
-        command ="{motor},{resolution},{direction},{steps},{velocity},{acceltime},{backlash}\n"
+        command ="{2},{self.motor2.res},{0},{self.dpad.lr_lineedit.text()},{self.motor2.velo},{self.motor2.acc},{self.motor2.bac}\n" 
         self.esp32.write(command.encode())
 
     def right_clicked(self):
-        resolition,velocity,acceltime,backlash = self.get_settings(self.motor2)
-        steps = self.dpad.lr_lineedit.text()
-        motor="2"
-        direction="1"
-        command ="{motor},{resolution},{direction},{steps},{velocity},{acceltime},{backlash}\n"
+        command ="{2},{self.motor2.res},{1},{self.dpad.lr_lineedit.text()},{self.motor2.velo},{self.motor2.acc},{self.motor2.bac}\n" 
         self.esp32.write(command.encode())
 
     def near_clicked(self):
-        resolition,velocity,acceltime,backlash = self.get_settings(self.motor3)
-        steps = self.dpad.nf_lineedit.text()
-        motor="3"
-        direction="0"
-        command ="{motor},{resolution},{direction},{steps},{velocity},{acceltime},{backlash}\n"
+        command ="{3},{self.motor3.res},{0},{self.dpad.nf_lineedit.text()},{self.motor3.velo},{self.motor3.acc},{self.motor3.bac}\n" 
         self.esp32.write(command.encode())
 
     def far_clicked(self):
-        resolition,velocity,acceltime,backlash = self.get_settings(self.motor3)
-        steps = self.dpad.nf_lineedit.text()
-        motor="3"
-        direction="1"
-        command ="{motor},{resolution},{direction},{steps},{velocity},{acceltime},{backlash}\n"
+        command ="{3},{self.motor3.res},{1},{self.dpad.nf_lineedit.text()},{self.motor3.velo},{self.motor3.acc},{self.motor3.bac}\n" 
         self.esp32.write(command.encode())
 
     def track_clicked(self):
-
+        #if !track.isChecked(): closee thread
+        t0 = time.time()
         # x,y = self.get_position()
         #imgs = get_imgs(ROI, exposure_time, gain, num_images, update=True)
         #for i in range (1, imgs.shape[0]):
@@ -218,31 +232,37 @@ class MainWindow(QMainWindow):
             # self.trajectory[self.tracked_points+1][1]+=self.trajectory[self.tracked_points][1]+dy
             # self.trajectory[self.tracked_points+1][2]+=self.trajectory[self.tracked_points][2]+time.time()
 
-        #function(t) = predict_trajectory(self.trajectory)
+        #predict(t) = predict_trajectory(self.trajectory)
+        #t_overhead = 0.5 
+        #x,y = predict(t_overhead+time.time()-t0) 
+        #command ="{1},{200},{x>1?1:0},{abs(x)},{300},{0},{backlash}\n"
 
         # while tracking.isChecked():
-            #thread_Start(period=5):
-            #
-            #get tracking_image (ROI,exposure time, gain, 1, show=False)
-            #calculate and append offse to trajecotry, image registration
-
-
-
-
-
+            #start_thread(period=5):
+            #when available:
+                #get tracking_image (ROI,exposure time, gain, 1, show=False)
+                #calculate and append dx, dy, t to self.trajectory. 
+                #update predict(t) = predict_trajectory(self.trajectory)
 
         #plot X vs t, y vs t, X vs Y
 
-        #
         pass
 
-    def get_settings(self, motor):
-        #resolutiom, steps,velocity,acceltime,backlash
+    def connect_camera(self):
+        #TODO: 
         pass
 
     def predict_trajectory(self, data_points, extrapolated_num):
 
-        #given [x,y,t] build a function as a function of time that best fits the trajectory 
+        #TODO: given [x,y,t] build a function as a function of time that best fits the trajectory 
+        pass
+        
+    def check_idle(self, period=600):
+        #TODO: if no motion command has been issued in the last 10 minutes, disable motors
+        pass
+        
+    def send_ascii_art(self,img):
+        #TODO: send ascii representation of the live image resized to 30x30
         pass
 
 if __name__ == '__main__':
